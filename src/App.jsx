8b5f90e6763cc2661b59b777528bb56da8cc9b15
Suzanne Atkinson <suzanneatkinson@gmail.com>
2024-02-14 21:54:13 -0500

    simple app

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import React, {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import About from "./components/About.jsx";
-import Home from "./components/Home.jsx";
-import TodoList from "./components/TodoList.jsx";
-import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
     const [showAbout, setShowAbout] = useState(true)
-    const [showHome, setShowHome] = useState(false)
-    const [showTodoList, setShowTodoList] = useState(false)
 
 
     return (
         <div>
+            <button
+                onClick={() => {
+                    setShowAbout(!showAbout)
+                }}
+            >About
+            </button>
 
-            <Sidebar
-                setShowAbout={setShowAbout}
-                setShowHome={setShowHome}
-                setShowTodoList={setShowTodoList}
-            />
 
             {  showAbout && <About/>  }
-
-          <Home showHome={showHome} setShowHome={setShowHome} setShowAbout={setShowAbout} />
-
-            {showTodoList && <TodoList/>}
 
         </div>
     )
